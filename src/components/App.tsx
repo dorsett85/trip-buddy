45f567bfe010a4dc793268c3ea8bdf8550af0916
clayton <claytonphillipsdorsett@gmail.com>
2019-08-24 23:18:10 -0400

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TripMap from './TripMap/TripMap';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <TripMap />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/TripMap/TripMap.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TripMap: React.FC = () => {
+  return (
+    <div>this will be the app</div>
+  )
+}
+
+export default TripMap;
