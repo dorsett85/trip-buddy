6f53b874e8ed519bce4eb1e0fa7227366cd2f66c
clayton <claytonphillipsdorsett@gmail.com>
2019-10-20 02:08:08 -0400

    Adds backend constants

---
+++ backend/utils/constants/errors.ts
@@ -0,0 +1,5 @@
+/* Define all error messages here */
+
+export const INTERNAL_SERVER_ERROR = 'INTERNAL_SERVER_ERROR';
+export const INTERNAL_SERVER_ERROR_MESSAGE =
+  'There was an error processing your request, please try again later';
