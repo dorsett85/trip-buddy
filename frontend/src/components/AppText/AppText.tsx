d1b74175f0a227c5dd2f1f6f6db46c212dbd3201
clayton <claytonphillipsdorsett@gmail.com>
2020-03-18 08:09:49 -0400

    Update AppText with defaults

--- frontend/src/components/AppText/AppText.tsx
+++ frontend/src/components/AppText/AppText.tsx
@@ -6,19 +6,19 @@ type TextTags = 'a' | 'span' | 'div' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 
 
 export interface AppTextProps {
   text: string;
-  color: keyof typeof theme['colors'];
-  element: TextTags | undefined;
+  color?: keyof typeof theme['colors'];
+  element?: TextTags;
 }
 
 const Text = styled.div<Pick<AppTextProps, 'color'>>(
   ({ color }) => css`
-    color: ${color};
+    color: ${color || 'inherit'};
   `
 );
 
-const AppText: React.FC<AppTextProps> = ({ text, color, element }) => {
+const AppText: React.FC<AppTextProps> = ({ text, color, element = 'span' }) => {
   return (
-    <Text as={element || 'span'} color={color}>
+    <Text as={element} color={color}>
       {text}
     </Text>
   );
