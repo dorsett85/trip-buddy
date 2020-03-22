7f5a06936e9df5209097279bda1c46b1fc328c54
clayton <claytonphillipsdorsett@gmail.com>
2020-03-21 22:17:51 -0400

    Reorder imports

--- backend/services/User.ts
+++ backend/services/User.ts
@@ -1,10 +1,10 @@
 import { UserRecord } from 'common/lib/types/user';
-import {TripInviteRecord} from "common/lib/types/tripInvite";
-import {TripRecord} from "common/lib/types/trip";
+import { TripInviteRecord } from 'common/lib/types/tripInvite';
+import { TripRecord } from 'common/lib/types/trip';
 import UserModel from '../models/User';
 import { UserServiceDeps } from './User.types';
 // eslint-disable-next-line import/no-cycle
-import {CreateTripInvitesInput, UpdateUserInput} from '../schema/resolvers/user.types';
+import { CreateTripInvitesInput, UpdateUserInput } from '../schema/resolvers/user.types';
 
 export default class UserService {
   private readonly user: UserRecord;
@@ -21,23 +21,23 @@ export default class UserService {
     return this.UserModel.findOne({ items: { id } });
   }
 
-  public updateOne(
-    updateUserInput: UpdateUserInput['input']
-  ): Promise<number> {
+  public updateOne(updateUserInput: UpdateUserInput['input']): Promise<number> {
     const { id } = this.user;
     return this.UserModel.updateOne(updateUserInput, { items: { id } });
   }
-  
+
   public possibleTripInvitees(tripId: TripRecord['id']): Promise<UserRecord[]> {
     return this.UserModel.findTripInvitees(tripId);
   }
-  
-  public async createTripInvites(invites: CreateTripInvitesInput['input']): Promise<TripInviteRecord['id'][]> {
+
+  public async createTripInvites(
+    invites: CreateTripInvitesInput['input']
+  ): Promise<TripInviteRecord['id'][]> {
     const invitesWithInvitee = invites.map(invite => {
       return {
         inviter_id: this.user.id,
         ...invite
-      }
+      };
     });
     const tripInvites = await this.UserModel.createTripInvites(invitesWithInvitee);
     return tripInvites.map(invite => invite.id);
