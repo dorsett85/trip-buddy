70dcc99f91fd71e2a64e069d7ef5eea7a65262df
clayton <claytonphillipsdorsett@gmail.com>
2020-04-27 21:44:10 -0400

    Delete gqlSchema types in common package

--- common/lib/types/gqlSchema/user.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserRecord, PartialUserRecord } from '../user';
-import { InputArgs } from './inputArgs';
-export declare type CreateUserArgs = Pick<UserRecord, 'username' | 'password' | 'email'>;
-export declare type CreateUserInputArgs = InputArgs<CreateUserArgs>;
-export declare type UpdateUserArgs = Omit<PartialUserRecord, 'id' | 'email_verification_token' | 'created_date'>;
-export declare type UpdateUserInputArgs = InputArgs<UpdateUserArgs>;
-//# sourceMappingURL=user.d.ts.map
