5c56c863ee2b45a3490438c1f6481b7c5117691f
clayton <claytonphillipsdorsett@gmail.com>
2020-05-07 08:27:08 -0400

    Add server production check

--- backend/server.ts
+++ backend/server.ts
@@ -3,7 +3,7 @@ import { ApolloServer } from 'apollo-server-express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import { createServer } from 'http';
-import { expressServer } from './config/config';
+import { expressServer, env } from './config/config';
 import { config } from './schema';
 
 // Express app
@@ -19,11 +19,13 @@ const server = new ApolloServer(config);
 server.applyMiddleware({ app });
 server.installSubscriptionHandlers(httpServer);
 
-// Serve static assets if not on the webpack dev server
-// app.use('/', express.static(path.resolve(__dirname, '../frontend/build')));
-// app.use('/', (req: any, res: any) => {
-//   res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-// });
+// Serve static assets in production
+if (env === 'production') {
+  app.use('/', express.static(path.resolve(__dirname, '../frontend/build')));
+  app.use('/', (req: any, res: any) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+  });
+}
 
 httpServer.listen(expressServer.port, () =>
   console.log(`App listening on port ${expressServer.port}!`)
